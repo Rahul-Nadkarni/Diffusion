72b774b8e73b84e25aa5bb4dd3065c2f8bbb27b6
Rahul-Nadkarni <125631652+Rahul-Nadkarni@users.noreply.github.com>
2023-02-20 20:15:58 -0800

    Add files via upload

--- modules.py
+++ modules.py
@@ -129,19 +129,23 @@ class UNet(nn.Module):
         super().__init__()
         self.device = device
         self.time_dim = time_dim
+
+        super().__init__()
+        self.device = device
+        self.time_dim = time_dim
         self.inc = DoubleConv(c_in, 64)
         self.down1 = Down(64, 128)
         self.sa1 = SelfAttention(128, 32)
-        self.down2 = Down(128, 256)
-        self.sa2 = SelfAttention(256, 16)
-        self.down3 = Down(256, 256)
-        self.sa3 = SelfAttention(256, 8)
+        self.down2 = Down(128, 128)
+        self.sa2 = SelfAttention(128, 16)
+        self.down3 = Down(128, 128)
+        self.sa3 = SelfAttention(128, 8)
 
-        self.bot1 = DoubleConv(256, 512)
-        self.bot2 = DoubleConv(512, 512)
-        self.bot3 = DoubleConv(512, 256)
+        self.bot1 = DoubleConv(128, 256)  # 128 256
+        self.bot2 = DoubleConv(256, 256)  # 256 256
+        self.bot3 = DoubleConv(256, 128)  # 256 128
 
-        self.up1 = Up(512, 128)
+        self.up1 = Up(256, 128)
         self.sa4 = SelfAttention(128, 16)
         self.up2 = Up(256, 64)
         self.sa5 = SelfAttention(64, 32)
@@ -197,13 +201,13 @@ class UNet_conditional(nn.Module):
         self.sa2 = SelfAttention(256, 16)
         self.down3 = Down(256, 256)
         self.sa3 = SelfAttention(256, 8)
-        self.down4 = Down(256,128)
-        self.sa9 = SelfAttention(128,4)
 
-        self.bot0 = DoubleConv(128,256)
-        self.bot4 = DoubleConv(256,128)
+        self.bot1 = DoubleConv(256, 512)
+        self.bot2 = DoubleConv(512, 512)
+        self.bot3 = DoubleConv(512, 256)
 
-
+        self.up1 = Up(512, 128)
+        self.sa4 = SelfAttention(128, 16)
         self.up2 = Up(256, 64)
         self.sa5 = SelfAttention(64, 32)
         self.up3 = Up(128, 64)
